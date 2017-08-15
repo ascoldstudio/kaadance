de02d6eb5b36801cdf7a291b47308deda19e63d9
Ascold1 <ascold96@gmail.com>
2017-08-16 00:09:45 +0300

    adaptive 3

--- app/js/common.js
+++ app/js/common.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
    // Пользовательские функции 
     $(" .gallery__photo").fancybox();
+    $(function(){
         var offset_left = function(val, area){
 
             var result = area.offset().left + area.width() - val;
@@ -11,27 +12,32 @@ $(document).ready(function(){
             var result = area.offset().top - val;
             return result;
         }
-        var area = $(".forms_main");
+        var area = $(".forms_main");/*
         var prize_area = $(".prize_container");
-       var news_area =$(".news_container");
+       var news_area =$(".news_container");*/
         $(window).resize(function(){
             $(".two_section__img").offset({ 
                 top: offset_top(50, area),
                 left: offset_left(150, area),
-            });
+            });/*
             $(".prize_bg__img.left").offset({
                 top:offset_top(-102, prize_area ),
                 left: offset_left(prize_area.width()+130, prize_area )
-            });
+            });*/
         });
         $(".two_section__img").offset({ 
             top: offset_top(50, area),
             left: offset_left(150, area),
         });
+    /*
         $(".prize_bg__img.left").offset({
             top:offset_top(-102, prize_area ),
             left: offset_left(prize_area.width()+130, prize_area )
-        });
+        });*/
+    })
+        
       
-       $(".header__menu").slicknav();
+       $(".header__menu").slicknav({
+           label: "",
+       });
 });
