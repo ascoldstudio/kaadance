6d95554b3cf1d7753b323b214cd86fdb1d704158
Ascold1 <ascold96@gmail.com>
2017-08-18 21:02:34 +0300

    final upd 1

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,11 @@
 ;"use strict";
 $(document).ready(function(){
    // Пользовательские функции 
-    $(" .gallery__photo").fancybox();
-    $(function(){
-       
-    })
-        
-      
+    $(" .gallery__photo").fancybox({
+                 
+                   allowfullscreen: 'true'
+                });
+   
        $(".header__menu").slicknav({
            label: "",
        });
